6a829383170b2fabe343c49aa573d5fbaefd3575
Yan Cervantes <acervantes@ia.com.mx>
2024-08-13 01:01:58 +0200

    **Feature:** public struct **Description:**  **Project:**     Cinepolis Redisign

--- Sources/SwiftNetwork/RequestConfiguration.swift
+++ Sources/SwiftNetwork/RequestConfiguration.swift
@@ -11,4 +11,10 @@ public struct RequestConfiguration: EndPoint {
   public var path: String
   public var bodyRequest: String?
   public var request: URLRequest
+  
+  public init(path: String, bodyRequest: String? = nil, request: URLRequest) {
+    self.path = path
+    self.bodyRequest = bodyRequest
+    self.request = request
+  }
 }
